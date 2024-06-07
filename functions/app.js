fa52efbd14f01e5386ecfab3f4a7d377956c3bb4
ahmed422-work <sameerahamed.work@gmail.com>
2024-06-07 20:02:23 +0530

    git igonered node module

--- functions/app.js
+++ functions/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const app = express();
 require('dotenv').config({path:'config.env'});
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const ejs = require('ejs');
-const {conn} = require('./middelwer/db');
+const {conn} = require('../middelwer/db');
 const session = require('express-session');
 const flash = require('connect-flash');
 var cors = require('cors');
@@ -14,8 +15,6 @@ const setTZ = require('set-tz') ;
 
 
 
-
-const PORT = process.env.PORT || 3000;
 conn.connect((err)=>{
     if (err) throw err;
     console.log('DB Connected !!!!');
@@ -30,13 +29,14 @@ app.use((req, res, next) => {
     next();
 });
 
-const loginRouter = require('./routers/login')
-const coustomerRouter = require('./routers/coustomer');
-const expenseRouter = require('./routers/expense');
-const toolROuter = require('./routers/tool');
-const servicesRouter = require('./routers/services');
-const reports = require('./routers/reports');
-const accountRouter = require('./routers/account');
+const loginRouter = require('../routers/login')
+const coustomerRouter = require('../routers/coustomer');
+const expenseRouter = require('../routers/expense');
+const toolROuter = require('../routers/tool');
+const servicesRouter = require('../routers/services');
+const reports = require('../routers/reports');
+const accountRouter = require('../routers/account');
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -87,10 +87,10 @@ app.use('/tool', toolROuter);
 app.use('/services', servicesRouter);
 app.use('/report', reports);
 app.use('/account', accountRouter);
-app.use('/app', require('./routers/app_login'));
-app.use('/admin', require('./routers/pos'))
-app.use('/coupon', require('./routers/coupon'));
-app.use('/order', require('./routers/order'));
+app.use('/app', require('../routers/app_login'));
+app.use('/admin', require('../routers/pos'))
+app.use('/coupon', require('../routers/coupon'));
+app.use('/order', require('../routers/order'));
 app.use((req, res, next) => {
     console.log(`Hostname: ${req.hostname}`);
     next();
@@ -98,6 +98,5 @@ app.use((req, res, next) => {
 
 
 // init port
-app.listen(PORT, ()=>{
-    console.log(`server running on port ${PORT}`);
-})
+app.use('/.netlify/functions/app', router);
+module.exports.handler = serverless(app);
